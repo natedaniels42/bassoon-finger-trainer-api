e774bda5c07b5d936618145997d08a66e951e54c
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-11 17:17:20 -0500

    Fixed the notes array in Bassoon controller

--- controllers/bassoon.js
+++ controllers/bassoon.js
@@ -1,5 +1,5 @@
 const db = require('../models');
-const notes = ['bflat1', 'b1', 'c2', 'dflat2', 'd2', 'eflat2', 'e2', 'f2', 'gflat2', 'g2', 'aflat2', 'a2', 'bflat2', 'b2', 'c3', 'dflat3', 'd3', 'eflat3', 'e3', 'f3', 'gflat3', 'g3', 'aflat3', 'a3', 'bflat3', 'b3', 'c4', 'dflat4', 'd4', 'eflat4', 'e4', 'f4', 'gflat4', 'g4', 'aflat4', 'a4']
+const notes = ['bflat1', 'b1', 'c2', 'dflat2', 'd2', 'eflat2', 'e2', 'f2', 'gflat2', 'g2', 'aflat2', 'a2', 'bflat2', 'b2', 'c3', 'dflat3', 'd3', 'eflat3', 'e3', 'f3', 'gflat3', 'g3', 'aflat3', 'a3', 'bflat3', 'b3', 'c4', 'dflat4', 'd4', 'eflat4', 'e4', 'f4', 'gflat4', 'g4', 'aflat4', 'a4', 'bflat4', 'b4', 'c5', 'dflat5', 'd5', 'eflat5', 'e5']
 
 const create = (req, res) => {
     db.Note.find({}, (err, foundNotes) => {
@@ -33,7 +33,7 @@ const findAnswers = (req, res) => {
         if (!randomNotes.includes(notes[randomNumber])) {
             randomNotes.push(notes[randomNumber]);
         }
-   }
+    }
 
     db.Note.find({name: { $in: randomNotes }}, (err, foundNotes) => {
         if (err) console.log(err);
